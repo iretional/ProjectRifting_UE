b8503d92d7561176102368e6f113c4aad0c19dd8
Ireh Choi <aisch1212@gmail.com>
2026-02-15 01:41:48 +0900

    new

---
+++ Source/ProjectRifting/ProjectRifting.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProjectRifting.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectRifting, "ProjectRifting" );
